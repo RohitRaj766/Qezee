6355b4b6c8997fa123a187867089205eca7b64d3
RohitRaj766 <rohitraj2k04@gmail.com>
2024-04-23 16:18:33 +0530

    .

---
+++ client/src/admin/components/login/LoginForm.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function LoginForm() {
+  return (
+    <>
+     <div className='login__box'>
+        <div className="admin__panel__heading">
+            <span>
+                Admin Panel
+            </span>
+        </div>
+
+            <form action="#">
+                <div className="user__id">
+                    <input type="text" placeholder='User Id'/>
+                </div>
+                <div className="password">
+                    <input type="password"  placeholder='Password'/>
+                </div>
+            </form>
+
+     </div>
+    </>
+  )
+}
+
+export default LoginForm
