73c4ec539b1d353855cd48dd3a90d00e51822c0d
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-07 18:41:54 +0530

    login showhide password

--- client/src/user/userScreens/login/Login.jsx
+++ client/src/user/userScreens/login/Login.jsx
@@ -4,9 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import { loginRequest } from '../../../actions/index';
 import './Login.scss';
 import pagePhoto from '../../assets/images/pagephoto.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
   const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch();
   const error = useSelector((state) => state.auth.error);
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -30,26 +33,52 @@ const Login = () => {
     }
   }, [isAuthenticated, navigate]);
 
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="loginMain">
       <div className="loginBoxContainer">
         <h1>Back in the Game! Let's Quiz!</h1>
         <div className='loginFormContainer'>
           <form>
-            <input className="loginInputField" type="email" name="email" value={credentials.email} onChange={handleChange} placeholder="Email*" required />
-            <input className="loginInputField" type="password" name="password" value={credentials.password} onChange={handleChange} placeholder="Password*" required />
+            <input
+              className="loginInputField"
+              type="email"
+              name="email"
+              value={credentials.email}
+              onChange={handleChange}
+              placeholder="Email*"
+              required
+            />
+            <div className="passwordField">
+              <input
+                className="loginInputField"
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={credentials.password}
+                onChange={handleChange}
+                placeholder="Password*"
+                required
+              />
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                onClick={toggleShowPassword}
+                className='showpassword'
+              />
+            </div>
           </form>
         </div>
         {error && <p className='errorMessage'>{error}</p>}
         <button onClick={handleSubmit} type="submit">LOGIN</button>
-        <p>forgot ? click here</p>
+        <p>Forgot? Click here</p>
       </div>
       <div className='backgroundImage'>
-        <img src={pagePhoto} alt="" />
+        <img src={pagePhoto} alt="Page Background" />
       </div>
     </div>
   );
 };
 
 export default Login;
-
