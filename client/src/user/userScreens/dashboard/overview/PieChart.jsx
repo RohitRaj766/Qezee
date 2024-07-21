ec019444dc0877693b991ed0eb63cfdba6b494e7
Preet2099x <preet2099x@gmail.com>
2024-07-21 07:19:48 +0530

    .

---
+++ client/src/user/userScreens/dashboard/overview/PieChart.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Chart from 'react-apexcharts';
+import './PieChart.scss';
+
+const PieChart = () => {
+  const [chartData, setChartData] = useState({
+    options: {
+      colors: ['#28a745', '#dc3545', '#007bff'],
+      legend: {
+        position: 'bottom'
+      },
+      labels: ['Correct', 'Incorrect', 'Not Attempted'],
+      plotOptions: {
+        pie: {
+            donut: {
+                size: '30%'
+            }
+        }
+      }
+    },
+    series: [54, 20, 26]
+  });
+
+  return (
+    <div className="pie">
+        <h2>Total Questions - 100</h2>
+      <div className="donut">
+        <Chart 
+          options={chartData.options}
+          series={chartData.series}
+          type="donut"
+          width="350"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PieChart;
