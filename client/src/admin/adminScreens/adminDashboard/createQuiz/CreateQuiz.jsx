4ba578e4f767744da819e92e3d66303bd5e1a5fb
Preet2099x <preet2099x@gmail.com>
2024-10-01 21:21:07 +0530

    .

--- client/src/admin/adminScreens/adminDashboard/createQuiz/CreateQuiz.jsx
+++ client/src/admin/adminScreens/adminDashboard/createQuiz/CreateQuiz.jsx
@@ -1,84 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react';
 import logout from '../../../../user/assets/images/logout.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutRequest } from '../../../../actions/index';
 import { useNavigate } from 'react-router-dom';
 import DatePicker from 'react-datepicker'; 
 import 'react-datepicker/dist/react-datepicker.css'; 
-import './CreateQuiz.scss'
+import './CreateQuiz.scss';
 
 function CreateQuiz() {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const admin = useSelector((state)=>state.adminauth.admin)
+  const admin = useSelector((state) => state.adminauth.admin);
+  
+  // State to manage questions
+  const [questions, setQuestions] = useState([
+    { question: '', options: ['', '', '', ''] }
+  ]);
+
+  // State for start and end date-time
+  const [startDateTime, setStartDateTime] = useState(null);
+  const [endDateTime, setEndDateTime] = useState(null);
+
+  // State for quiz status
+  const [isActive, setIsActive] = useState(true); // Default to Active
 
   const handleLogout = () => {
     dispatch(logoutRequest());
     navigate('/admin-login');
   };
 
+  const handleQuestionChange = (index, value) => {
+    const newQuestions = [...questions];
+    newQuestions[index].question = value;
+    setQuestions(newQuestions);
+  };
+
+  const handleOptionChange = (qIndex, oIndex, value) => {
+    const newQuestions = [...questions];
+    newQuestions[qIndex].options[oIndex] = value;
+    setQuestions(newQuestions);
+  };
+
+  const addQuestion = () => {
+    setQuestions([...questions, { question: '', options: ['', '', '', ''] }]);
+  };
+
+  // Toggle function for active/inactive state
+  const toggleActive = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div>
-
-    <div className='nav'>
-      <div className='nav-title'>
-        <h1>CREATE QUIZ</h1>
-      </div>
-      <img className='logout' src={logout} alt='' onClick={handleLogout} />
-    </div>
-
-    <div className="quiz-details">
-
-      <div className="title-status">
-        <input type="text" placeholder='Quiz Title' />
-        <button>Active </button>  {/* Add a toggle thing which shows active by default and when clicked changes to inactive */}
-      </div>
-      
-      <div className="time">
-        <div>
-          <label>Start Date:</label>
-          <DatePicker/>
+      <div className='nav'>
+        <div className='nav-title'>
+          <h1>CREATE QUIZ</h1>
         </div>
-        <div>
-          <label>End Date:</label>
-          <DatePicker/>
-        </div>
-
+        <img className='logout' src={logout} alt='' onClick={handleLogout} />
       </div>
 
-    </div>
-
-    <div className="questions-container">
-
-      <input type="text" placeholder='Question'/>
-
-      <div className='options'>
-
-        <div className='options1'>
-          <input type="text" placeholder='option 1'/>
-          <input type="text" placeholder='option 3'/>
+      <div className="quiz-details">
+        <div className="title-status">
+          <input type="text" placeholder='Quiz Title' className='quiz-title' />
         </div>
 
-        <div className="options2">
-          <input type="text" placeholder='option 2'/>
-          <input type="text" placeholder='option 4'/>
+        <div className="time">
+          <div>
+            <label>Start Date:</label>
+            <DatePicker
+              selected={startDateTime}
+              onChange={(date) => setStartDateTime(date)}
+              showTimeSelect
+              dateFormat="Pp" // Formats the date and time
+              timeFormat="HH:mm"
+            />
+          </div>
+          <div>
+            <label>End Date:</label>
+            <DatePicker
+              selected={endDateTime}
+              onChange={(date) => setEndDateTime(date)}
+              showTimeSelect
+              dateFormat="Pp" // Formats the date and time
+              timeFormat="HH:mm"
+            />
+          </div>
+          <button onClick={toggleActive} className='status'> 
+            {isActive ? 'Active' : 'Inactive'}
+          </button>
         </div>
-        
+      </div>
+
+      <div className="questions-container">
+        {questions.map((q, qIndex) => (
+          <div key={qIndex} className="question-container">
+            <input
+              type="text"
+              placeholder='Question'
+              className='question'
+              value={q.question}
+              onChange={(e) => handleQuestionChange(qIndex, e.target.value)}
+            />
+
+            <div className='options'>
+              {q.options.map((option, oIndex) => (
+                <input
+                  key={oIndex}
+                  type="text"
+                  placeholder={`Option ${oIndex + 1}`}
+                  value={option}
+                  onChange={(e) => handleOptionChange(qIndex, oIndex, e.target.value)}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="add-counter">
+        <p>Total Questions: {questions.length}</p>
+        <button onClick={addQuestion}>Add</button>
+      </div>
+
+      <div className='upload'>
+        <button>Upload</button>
       </div>
     </div>
-
-    <div className="add-counter">
-      <p>Total Questions: 1</p>
-      <button>Add</button>
-    </div>
-
-    <div className='upload'>
-      <button>Upload</button>
-    </div>
-
-    </div>
-  )
+  );
 }
 
-export default CreateQuiz
+export default CreateQuiz;
